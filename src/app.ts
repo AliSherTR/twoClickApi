86869c539fb4b1755444f23e3a876a840f4f2f5b
AliSherTR <alisherkhan817@gmail.com>
2025-07-30 15:57:55 +0500

    deep linking

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import authRoutes from "./routes/auth/auth.route";
@@ -8,12 +8,28 @@ import { globalErrorHandler } from "./middlewares/global-error-handler";
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(express.static("src"));
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
-app.use("/api/session", sessionRoutes);
+app.use("/session", sessionRoutes);
+
+app.get("/.well-known/assetlinks.json", (req: Request, res: Response) => {
+  res.status(200).json([
+    {
+      relation: ["delegate_permission/common.handle_all_urls"],
+      target: {
+        namespace: "android_app",
+        package_name: "com.click2.app",
+        sha256_cert_fingerprints: [
+          "89:14:04:F7:B6:18:0F:28:01:F2:D0:9E:37:C5:3A:34:3E:D5:A5:42:84:37:70:C6:50:31:3C:7B:10:02:5B:C1",
+        ],
+      },
+    },
+  ]);
+});
 
 app.use(globalErrorHandler);
 
